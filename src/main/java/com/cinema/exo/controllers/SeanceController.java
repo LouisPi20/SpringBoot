0445746d159f0e422988123f98c8e203e6ef7e67
Pivain <louis--76@hotmail.fr>
2020-04-04 19:58:28 +0200

    CRUD Seance

--- src/main/java/com/cinema/exo/controllers/SeanceController.java
+++ src/main/java/com/cinema/exo/controllers/SeanceController.java
@@ -1,6 +1,47 @@
 package com.cinema.exo.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cinema.exo.models.Seance;
+import com.cinema.exo.services.SeanceService;
+
+@CrossOrigin
+@RestController
+@RequestMapping("seances")
 public class SeanceController {
+	
+	@Autowired
+	private SeanceService service;
+	
+	@GetMapping("")
+	public List<Seance> findAll() {
+		return this.service.findAll();	
+	}
+	
+	@PostMapping("")
+	public Seance saveSeances(@RequestBody Seance seanceSave) {
+		return this.service.saveSeances(seanceSave);
+	}
+	
+	@PutMapping("")
+	public Seance modifierSeances(@RequestBody Seance seanceModif) {
+		return this.service.modifierSeances(seanceModif);
+	}
+	
+	@DeleteMapping("")
+	public void suppSeances(@RequestBody Seance seanceSupp) {
+		this.service.suppSeances(seanceSupp);
+	}
 	
 }
 
--- src/main/java/com/cinema/exo/services/SeanceService.java
+++ src/main/java/com/cinema/exo/services/SeanceService.java
@@ -1,5 +1,33 @@
 package com.cinema.exo.services;
 
-public class SeanceService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cinema.exo.models.Seance;
+import com.cinema.exo.repositories.SeanceRepository;
+
+
+@Service
+public class SeanceService {
+	
+	@Autowired
+	private SeanceRepository repository;
+
+	public List<Seance> findAll() {
+		return this.repository.findAll();
+	}
+
+	public Seance saveSeances(Seance seanceSave) {
+		return this.repository.save(seanceSave);
+	}
+	
+	public Seance modifierSeances(Seance seanceModif) {
+		return this.repository.save(seanceModif);
+	}
+	
+	public void suppSeances(Seance seanceSupp) {
+		this.repository.delete(seanceSupp);
+	}
 }
